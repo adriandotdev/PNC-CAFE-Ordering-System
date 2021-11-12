407d38cdfd69bb1433fa17a00b6017859af7d3f0
Adrian Nads <marcelonads@gmail.com>
2021-11-12 16:31:14 +0800

    changed the theme to light

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 
 function App() {
   return (
-    <div className="App">
+    <div className="App navbar h-7 bg-green-600">
       <h1>React Here</h1>
+      <h2>React Here</h2>
     </div>
   );
 }
