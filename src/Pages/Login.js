8b63592a533d15425a1989b0e3b56e62c046dade
Adrian Nads <marcelonads@gmail.com>
2021-11-13 00:00:43 +0800

    Done for Hero section for login page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -6,28 +6,40 @@ import Button from '../components/Button'
 
 function Login() {
     return (
-        <div className="h-screen flex justify-center items-center p-3">
-            <div className="max-w-md w-full m-auto">
-            <h1 className="font-bold text-xl md:text-2xl text-pnc">Login</h1>
-                <form className="form-control " action="">
-                
-                    {/* INPUT FIELDS */}
-                    <InputContainer name="id-number" type="text" labelContent="ID Number"/>
-                    <InputContainer name="password" type="password" labelContent="Password"/>
-
-                    {/* Login Button */}
-                    <section>
-                        <Button className="btn bg-pnc w-full hover:bg-pncHover text-lg font-medium text-white md:text-xl" text="Login"/>
-                    </section>
-
-                    {/* Link Section */}
-                    <section className="flex flex-col items-center self-center mt-2">
-                        <small className="text-pnc font-medium text-center sm:text-base">Don't have an account yet? <Link to="/" className="text-pncHover">Sign Up</Link> </small>
-                        
-                        <small className="text-pnc font-medium text-center sm:text-base" ><Link to="/">Forgot password?</Link></small>
-                    </section>
-                </form>
+        <div className="hero h-screen p-20 flex flex-col justify-center gap-4 lg:flex-row">
+            
+            {/* Title Section */}
+             <div className="hero-content flex-col items-center lg:items-start gap-3 flex-shrink-0">
+                    <h1 className="text-2xl text-pnc font-bold lg:text-4xl xl:text-6xl text-center">Welcome to PNC Cafe</h1>
+                    <p className="font-medium text-pnc text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
+
+            {/* Login Form */}
+            <div className="hero-content gap-6 max-w-4xl w-full ">
+               
+                <div className="card bordered border max-w-md w-full m-auto p-5 ">
+                    <h1 className="card-title font-bold text-xl md:text-2xl text-pnc">Login</h1>
+                    <form className="form-control " action="">
+                    
+                        {/* INPUT FIELDS */}
+                        <InputContainer name="id-number" type="text" labelContent="ID Number"/>
+                        <InputContainer name="password" type="password" labelContent="Password"/>
+
+                        {/* Login Button */}
+                        <section>
+                            <Button className="btn bg-pnc w-full hover:bg-pncHover text-lg font-medium text-white md:text-xl" text="Login"/>
+                        </section>
+
+                        {/* Link Section */}
+                        <section className="flex flex-col items-center self-center mt-2">
+                            <small className="text-pnc font-medium text-center sm:text-base">Don't have an account yet? <Link to="/" className="text-pncHover">Sign Up</Link> </small>
+                            
+                            <small className="text-pnc font-medium text-center sm:text-base" ><Link to="/">Forgot password?</Link></small>
+                        </section>
+                    </form>
+                </div>
+            </div>
+            
         </div>
     )
 }
