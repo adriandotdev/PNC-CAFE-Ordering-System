6e2774abbfe778f805ec322a7e31d302c68f937c
Adrian Nads <marcelonads@gmail.com>
2021-11-14 00:11:41 +0800

    Created a Sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div className="max-h-min row-start-1 col-start-1 col-end-3 grid grid-flow-col place-items-center place-content-center gap-5 p-3 shadow-md lg:shadow-none lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-4 lg:grid-flow-row lg:max-w-min lg:place-content-start">
+
+            <section className="max-w-min">
+                <button className="admin-drawer-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </button>
+            </section>
+            
+            <section className="max-w-min z-20">
+                <button className="admin-drawer-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </button>
+                
+            </section>
+            
+            <section className="max-w-min z-10">
+                <button className="admin-drawer-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                    </svg>
+                </button>
+                
+            </section>
+        </div>
+    )
+}
+
+export default Sidebar
