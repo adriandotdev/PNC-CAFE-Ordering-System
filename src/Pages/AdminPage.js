3380f38db033432bf8172441d1f45d97f5af8119
Adrian Nads <marcelonads@gmail.com>
2021-11-14 22:22:39 +0800

    Created an outlet inside the AdminPage as a Route for each buttons in the sidebar

--- src/Pages/AdminPage.js
+++ src/Pages/AdminPage.js
@@ -1,58 +1,14 @@
 import React from 'react'
-import {customer} from '../test/customer'
+import {Outlet} from 'react-router-dom'
 import Sidebar from '../components/Sidebar'
 
 function AdminPage() {
 
     return (
-        <div className="main-content lg:h-full lg:sticky">
+        <div className="main-content-admin lg:h-full lg:sticky">
             
             <Sidebar />
-
-            <div className="row-start-2 row-end-2 col-start-1 lg:col-start-2 lg:row-start-1 lg:row-end-1">
-                <h1 className="p-3 font-medium md:text-lg lg:text-2xl">Customers</h1>
-            </div>
-
-            <div className="lg:row-start-2 col-start-1 col-end-3 lg:col-start-2 lg:col-end-3 overflow-y-auto max-h-96 border">
-                
-                <table className="table w-full relative ">
-                    <thead className="relative">
-                            
-                            <th className="hidden"></th>
-                            <th className="table-headers">ID Number</th>
-                            <th className="table-headers">Given Name</th>
-                            <th className="table-headers">Middle Name</th>
-                            <th className="table-headers">Last Name</th>
-                            <th className="table-headers">Email</th>
-                        
-                        
-                    </thead>
-                    <tbody className="overflow-y-auto">
-                        {
-                            customer.map(cust => {
-                                return (
-                                    <tr className="hover">
-                                        <th className="hidden"></th>
-                                        <th>{cust.idNumber}</th>
-                                        <td>{cust.givenName}</td>
-                                        <td>Lauriano</td>
-                                        <td>Marcelo</td>
-                                        <td>email@gmail.com</td>
-                                    </tr>
-                                )
-                            })
-                        }
-                        
-                    </tbody>
-                    <tfoot className="sticky top-auto left-0 right-0">
-                        <th>ID Number</th>
-                        <th>Given Name</th>
-                        <th>Middle Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                    </tfoot>
-                </table>
-            </div>
+            <Outlet />
         </div>
     )
 }
