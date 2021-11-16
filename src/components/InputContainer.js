8dc60e7588c0b9d1cf9086dcd01e93f35c2fc28b
Adrian Nads <marcelonads@gmail.com>
2021-11-16 17:20:47 +0800

    added a value and onChange props to InputContainer component

--- src/components/InputContainer.js
+++ src/components/InputContainer.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Textfield  from './Textfield'
 import Label from './Label'
 
-function InputContainer({name, type, labelContent}) {
+function InputContainer({name, type, labelContent, value, onChange}) {
     return (
         <section className="input-container flex flex-col-reverse relative">
 
-            <Textfield props={{name, type}}/>
+            <Textfield props={{name, type}} value={value} onChange={onChange}/>
             <Label props={{labelContent, name}} />
         </section>
     )
