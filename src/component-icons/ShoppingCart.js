9fc73d01e3817eae7f40a6b989eae59012f77f2a
Adrian Nads <marcelonads@gmail.com>
2021-11-17 09:54:41 +0800

    changed the icon from shopping cart to shopping bag

--- src/component-icons/ShoppingCart.js
+++ src/component-icons/ShoppingCart.js
@@ -11,7 +11,7 @@ function ShoppingCart() {
             <div className="indicatior-item badge absolute right-0 z-10 bg-pncHover border-none">0</div>
             <label htmlFor="cart" onClick={() => setClicked(!clicked)} className="button">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 md:h-8 md:w-8" fill="none" viewBox="0 0 24 24" stroke="white">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                 </svg>
             </label>  
 
