5ba50940469c9bd497e374c04f679d4c31e870a9
Adrian Nads <marcelonads@gmail.com>
2021-11-18 17:57:02 +0800

    Use an AdminMenuContext
    set a function for deleting a specific menu
    
    Adds an event for each modal button

--- src/components (admin)/DeleteModal.js
+++ src/components (admin)/DeleteModal.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {AdminMenuContext} from '../contexts/AdminMenuContext'
 import ModalButton from './ModalButton'
 
 function DeleteModal() {
+
+    const [ menuID, setMenuID ] = useContext(AdminMenuContext)
+    async function deleteMenu() {
+
+        const res = await fetch('http://localhost:3001/delete-menu', { 
+            method: 'DELETE', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({menuID})
+        })
+    }
     return (
         <>  
             <div className="modal">
@@ -10,8 +23,17 @@ function DeleteModal() {
                     <p>Are you sure you want to delete this menu?</p>
 
                     <div className="modal-action">
-                        <ModalButton htmlFor="delete-modal" className="admin-delete-btn modal-button" text="Confirm"/>
-                        <ModalButton htmlFor="delete-modal" className="admin-cancel-btn modal-button" text="Cancel"/>
+
+                        <ModalButton onClick={() => {
+
+                            deleteMenu();
+                            setMenuID('')
+                        }} htmlFor="delete-modal" className="admin-delete-btn modal-button" text="Confirm"/>
+
+                        <ModalButton onClick={() => {
+
+                            setMenuID('')
+                        }} htmlFor="delete-modal" className="admin-cancel-btn modal-button" text="Cancel"/>
                     </div>
                 </div>
             </div>
