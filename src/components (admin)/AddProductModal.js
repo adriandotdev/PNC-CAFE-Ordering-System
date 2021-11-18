06d3c1f4b3ca13184d20d04372af4812ddc9b2f8
Adrian Nads <marcelonads@gmail.com>
2021-11-18 17:54:24 +0800

    Use an AdminMenuContext
    done for creating a functionality about the add, delete, and update of menu

--- src/components (admin)/AddProductModal.js
+++ src/components (admin)/AddProductModal.js
@@ -1,37 +1,96 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
+import {AdminMenuContext} from '../contexts/AdminMenuContext'
 import Textfield from '../components/Textfield'
 import Label from '../components/Label'
 import Button from '../components/Button';
 import ModalButton from './ModalButton'
 
-function AddProductModal({isEditing, setEditing}) {
+function AddProductModal() {
 
-    const [menu, setMenu] = useState('');
+    const [menuID, setMenuID, isEditing, setEditing] = useContext(AdminMenuContext) // Context of Admin Page
+    
+    const [menuName, setMenuName] = useState('');
     const [menuDesc, setMenuDesc] = useState(''); // menu description
     const [price, setPrice] = useState(0);
+    
+    /**
+     * This is the function that adds the new menu to the database. */
+    async function addMenu() {
+
+        const image = document.querySelector('#image').value;
+
+        const res = await fetch('http://localhost:3001/add-menu', { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify( { id: Date.now().toLocaleString(), menuName, menuDesc, price, image } )
+        })
+
+        setMenuID('')
+    }
 
     /**
-     * Dito ay mag po-POST request ka
-     * para mag add ng new menu.
-     * 
-     * then pag mag eedit ka mag po-POST request ka din
-     */
+     * This function will fetch the menu based on menu id. */
+    async function getMenuForUpdate() {
+
+        const res = await fetch('http://localhost:3001/get-menu-id', { 
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({menuID})
+        })
+
+        const data = await res.json()
+
+        // This will setup the value of the selected menu to be edited.
+        const menu = JSON.parse(data)
+        setMenuName(menu[0]['menu'])
+        setMenuDesc(menu[0]['menu_desc'])
+        setPrice(menu[0]['menu_price'])
+    }
+
+    /** A function that updates a selected menu. */
+    async function update() {
+
+        const image = document.querySelector('#image').value; 
+
+        const res = await fetch('http://localhost:3001/edit-menu', { 
+
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({menuID, menuName, menuDesc, price, image})
+        })
+    }
+
+    /** A useEffect that triggers whenever 
+     * the admin wants to edit a selected menu. */
+    useEffect(() => {
+
+        if (menuID)
+            getMenuForUpdate();
+
+    }, [isEditing])
+
     return (
         <>
             
             <div className="modal ">
                 
-                <form className="modal-box">
+                <form className="modal-box" autoComplete="off" onSubmit={(e) => e.preventDefault()}>
 
-                    <h1 className="font-bold text-2xl lg:text-3xl">{isEditing ? "Edit Product" : "Add Product"}</h1>
+                    <h1 className="font-bold text-2xl lg:text-3xl">{isEditing ? "Edit Menu" : "Add Menu"}</h1>
                     
                     <section className="input-container flex flex-col-reverse relative">
-                        <Textfield props={{type: 'text', name: 'product-name'}} value={menu} onChange={(e) => setMenu(e.target.value)}/>
+                        <Textfield props={{type: 'text', name: 'product-name'}} value={menuName} onChange={(e) => setMenuName(e.target.value)}/>
                         <Label props={{name: 'product-name', labelContent: 'Product Name'}} />
                     </section>
 
                     <div className="form-control">
-                        <textarea value={menuDesc} onChange={(e) => setMenuDesc(e.target.value)} 
+                        <textarea value={menuDesc} id="product-desc" name="product-desc" onChange={(e) => setMenuDesc(e.target.value)} 
                         className="textarea border-pnc textarea-bordered focus:ring-1 ring-pnc" placeholder="Product Description" required></textarea>
                     </div>
 
@@ -40,10 +99,36 @@ function AddProductModal({isEditing, setEditing}) {
                         <Label props={{name: 'product-price', labelContent: 'Product Price'}} />
                     </section>
 
+                    <input type="file" name="image" id="image" />
+                    
                     <div className="modal-action">
-                        <Button className="admin-sidebar-btn" text={isEditing ? "Edit" : "Add"}/>
+                        <ModalButton htmlFor="menu-modal" className="admin-sidebar-btn" text={isEditing ? "Edit" : "Add"} 
+                        
+                        onClick={ !isEditing ? () => {
+
+                            // This is the function when the user is adding a new menu.
+                                addMenu()
+                                setMenuName('')
+                                setMenuDesc('')
+                                setPrice(0)
+                                setMenuID('')
+                            } : () => {
+                            
+                            // This is the function when the user is adding a new menu.
+                                update()
+                                setMenuName('')
+                                setMenuDesc('')
+                                setPrice(0)
+                                setMenuID('')
+                                setEditing(false)
+                            }
+                        }/>
                         <ModalButton htmlFor="menu-modal" className="admin-sidebar-btn modal-button" text="Close" onClick={() => { 
                             setEditing(false)
+                            setMenuName('')
+                            setMenuDesc('')
+                            setPrice(0)
+                            setMenuID('')
                         }}/>
                     </div>
                 </form>
