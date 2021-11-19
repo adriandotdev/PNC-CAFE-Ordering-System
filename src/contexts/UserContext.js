0746812c49bcb796075c42d66fad70b106017c83
Adrian Nads <marcelonads@gmail.com>
2021-11-19 09:00:40 +0800

    added a UserContext that holds the userIDNumber, menuID for showing it on menu info modal, then a quantity for each individual menu

---
+++ src/contexts/UserContext.js
@@ -0,0 +1,17 @@
+import React, {createContext, useState} from 'react'
+
+export const UserContext = createContext()
+
+// This will wrap up all the children components
+export function UserProvider(props) {
+
+    const [userIDNumber, setUserIDNumber] = useState(null);
+    const [menuID, setMenuID] = useState('')
+    const [quantity, setQuantity] = useState(1)
+
+    return (
+        <UserContext.Provider value={[userIDNumber, setUserIDNumber, menuID, setMenuID, quantity, setQuantity]}>
+            {props.children}
+        </UserContext.Provider>
+    )
+}
