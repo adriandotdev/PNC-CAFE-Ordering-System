158fb5b76be6d51d7cf571cde48ca2b45282638e
Adrian Nads <marcelonads@gmail.com>
2021-11-20 11:42:59 +0800

    wrapped the content of Navbar.js of <UserProvider>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,27 +2,30 @@ import React from 'react'
 import {Outlet} from 'react-router-dom'
 import ShoppingCart from '../component-icons/ShoppingCart'
 import Dropdown from '../component-icons/Dropdown'
+import {UserProvider} from '../contexts/UserContext'
 
 function Navbar({navClass, isUser, setUser}) {
     return (
-        <div className={navClass}>
+        <UserProvider>
+                <div className={navClass}>
 
-            <div className="relative">
-                <nav className="nav navbar bg-pnc p-5 navbar-start w-full flex items-center justify-between fixed top-0 z-10">
-                    <h1 className="text-white font-bold text-2xl md:text-3xl ">PNC Cafe</h1>
-                    
-                    <section className="">
+                <div className="relative">
+                    <nav className="nav navbar bg-pnc p-5 navbar-start w-full flex items-center justify-between fixed top-0 z-10">
+                        <h1 className="text-white font-bold text-2xl md:text-3xl ">PNC Cafe</h1>
+                        
+                        <section className="">
 
-                        { isUser && <ShoppingCart /> }
-                        <Dropdown isUser={isUser} setUser={setUser}/>
-                    </section>
-                </nav>
+                            { isUser && <ShoppingCart />}
+                            <Dropdown isUser={isUser} setUser={setUser}/>
+                        </section>
+                    </nav>
+                </div>
+                
+
+                {/* Other Pages to be rendered (E.g. UserLoginPage, UserSignupPage) */}
+                <Outlet />
             </div>
-            
-
-            {/* Other Pages to be rendered (E.g. UserLoginPage, UserSignupPage) */}
-            <Outlet />
-        </div>
+        </UserProvider>
     )
 }
 
