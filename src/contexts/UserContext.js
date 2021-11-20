76455c1efe8e3c800da6d1de459f1d530f1a44b9
Adrian Nads <marcelonads@gmail.com>
2021-11-20 11:41:08 +0800

    change the initial value of userIDNumber and change the variable name of cartMenus to 'noOfCartItems'

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -5,13 +5,13 @@ export const UserContext = createContext()
 // This will wrap up all the children components
 export function UserProvider(props) {
 
-    const [userIDNumber, setUserIDNumber] = useState(null);
+    const [userIDNumber, setUserIDNumber] = useState('');
     const [menuID, setMenuID] = useState('')
     const [quantity, setQuantity] = useState(1)
-    const [cartMenus, setCartMenus] = useState(0)
+    const [noOfCartItems, setNoOfCartItems] = useState(0)
     
     return (
-        <UserContext.Provider value={[userIDNumber, setUserIDNumber, menuID, setMenuID, quantity, setQuantity]}>
+        <UserContext.Provider value={[userIDNumber, setUserIDNumber, menuID, setMenuID, quantity, setQuantity, noOfCartItems, setNoOfCartItems]}>
             {props.children}
         </UserContext.Provider>
     )
