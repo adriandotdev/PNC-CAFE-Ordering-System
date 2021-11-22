e3fa7cc652aca29b731f25aef8b71b4f1a77f952
Adrian Nads <marcelonads@gmail.com>
2021-11-22 13:09:21 +0800

    Done for setting up the functionality of disabling and enabling the textfield when the user wants to edit his/her profile information / account information

--- src/components/ProfileInput.js
+++ src/components/ProfileInput.js
@@ -1,31 +1,34 @@
 import React from 'react'
 
-function ProfileInput({inputDisabled, setInputDisabled, value, setValue}) {
+function ProfileInput({inputDisabled, onEdit, onCancel, value, onChange, htmlFor, type}) {
+
     return (
-        <>
-            <input disabled={inputDisabled} value={value} onChange={(e) => setValue(e.target.value)} className={inputDisabled ? "border border-green-400 outline-none p-2 w-full border-none" : "border border-green-400 outline-none p-2 w-full"} type="text" name="email" id="email" />
+        <section className="flex justify-stretch w-full gap-1 items-center">
+            <input 
+            disabled={type == "password" ? true : inputDisabled} 
+            value={value} 
+            onChange={onChange} 
+            // "border border-green-400 outline-none p-2 w-full border-none"
+            // "border border-green-400 outline-none p-2 w-full"
+            className={type == "password" ? "border border-green-400 outline-none p-2 w-full border-none" : inputDisabled ? "border border-green-400 outline-none p-2 w-full border-none" : "border border-green-400 outline-none p-2 w-full"}
+            type={type} name={htmlFor} id={htmlFor} 
+            required/>
 
                 {inputDisabled 
                 
-                    ?  <svg onClick={() => { 
-
-                        setInputDisabled(false)
-                    }} 
+                    ?  <svg onClick={onEdit} 
                     xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                     </svg>
                 
                     :
 
-                    <svg  onClick={() => {
-
-                        setInputDisabled(true)
-                    }} 
+                    <svg  onClick={onCancel} 
                     xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 }
-        </>
+        </section>
     )
 }
 
