a7fccc4fa7761c49c207961a783c2c57100d8399
Adrian Nads <marcelonads@gmail.com>
2021-11-22 13:10:30 +0800

    Change the shopping cart as a Link to reference the cart page

--- src/component-icons/ShoppingCart.js
+++ src/component-icons/ShoppingCart.js
@@ -28,7 +28,7 @@ function ShoppingCart() {
         <>
         <div className="indicator relative">
             <div className="indicatior-item badge absolute right-0 z-10 bg-pncHover border-none">{noOfCartItems}</div>
-            <Link to="" onClick={() => setClicked(!clicked)} className="button">
+            <Link to="/homepage" onClick={() => setClicked(!clicked)} className="button">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 md:h-8 md:w-8" fill="none" viewBox="0 0 24 24" stroke="white">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                 </svg>
