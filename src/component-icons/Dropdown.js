3c9d81f60a9aa7c1f7ffe4d85a895e6192bc0290
Adrian Nads <marcelonads@gmail.com>
2021-11-22 13:08:27 +0800

    Changed the Button to Link to homepage

--- src/component-icons/Dropdown.js
+++ src/component-icons/Dropdown.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import Button from '../components/Button'
 
 // This is the dropdown menu for user.
@@ -6,15 +7,19 @@ function Dropdown({isUser, setUser}) {
     return (
         <>
             <div  className="dropdown dropdown-hover dropdown-end">
+
                 <button className="button">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 md:h-8 md:w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
                 </button>
+
                 <ul className="menu dropdown-content w-32 rounded mr-1 z-10 bg-pncHover transition-all shadow-2xl">
 
+                    {isUser && <Link to="/profile" className="btn-block focus:border-none hover:bg-pnc p-2 font-medium transition-all text-white text-center">Profile</Link>}
+
                     {/* Menu Button */}
-                    <Button className="btn-block focus:border-none hover:bg-pnc p-2 font-medium transition-all text-white" text="Menu"/>
+                    <Link to="/homepage" className="text-center btn-block focus:border-none hover:bg-pnc p-2 font-medium transition-all text-white">Menu</Link>
 
                     {/* About Button */}
                     <Button className="btn-block hover:bg-pnc p-2 font-medium transition-all text-white" text="About"/>
