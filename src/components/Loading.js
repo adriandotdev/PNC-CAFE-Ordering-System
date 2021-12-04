2964b22c3ab55f3735ead1100470424506a3cea1
Adrian Nads <marcelonads@gmail.com>
2021-12-04 18:10:11 +0800

    Added a Loading Component

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loading() {
+    return (
+        <>
+            <button className="btn loading btn-circle btn-lg bg-base-200 btn-ghost"></button>
+        </>
+    )
+}
+
+export default Loading
