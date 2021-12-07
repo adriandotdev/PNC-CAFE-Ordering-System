c464c061e7bd89e04fc7816e0e79c7747b3b17d5
Adrian Nads <marcelonads@gmail.com>
2021-12-07 10:14:51 +0800

    FIXED THE BUG
    The bug is whenever we view the invoice of each individual items, the subtotal is not showing properly
    
    FIXED BY PUTTING the orderDetails props in the dependency list of the useEffect

--- src/Pages/YourOrders.js
+++ src/Pages/YourOrders.js
@@ -1,25 +1,35 @@
 import React, {useState, useEffect, useContext} from 'react'
+import {UserProvider} from '../contexts/UserContext'
 import {UserContext} from '../contexts/UserContext'
 import Button from '../components/Button'
+import Invoice from '../components/Invoice'
 
+// YourOrders Page
 function YourOrders() {
 
-    const {userIDNumber} = useContext(UserContext)
+    // User Context
+    const {isUser, setUser, userIDNumber, setUserIDNumber} = useContext(UserContext)
+    const [orderDetails, setOrderDetails] = useState(null)
     const [activeTab, setActiveTab] = useState({
 
-        pending: true,
-        cancelled: false,
-        received: false
+        toPrepare: true,
+        preparing: false,
+        toReceived: false,
+        received: false,
+        cancelled: false
     })
 
     const [orders, setOrders] = useState([])
 
-    const getPendingOrders = () => {
-        fetch('http://localhost:3001/pending-orders', {
+    /** A function that gets the orders 
+     * based on the specified status. */
+    const getOrdersWithStatus = (status) => {
+
+        fetch('http://localhost:3001/get-order-with-status', {
 
             method: 'POST',
             headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify({userIDNumber})
+            body: JSON.stringify({userIDNumber, status})
         })
         .then(res => res.json())
         .then(data => {
@@ -27,62 +37,139 @@ function YourOrders() {
         })
     }
 
-    const getReceivedOrders = () => {
-
+    /** A function that sets the order
+     * based on the specified status. */
+    const setOrderStatusAs = (status, orderID) => {
+        
+        fetch('http://localhost:3001/set-order-as', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({status, userIDNumber, orderID})
+        })
+        .then(response => response.json())
+        .then(_data => {
+            
+            getOrdersWithStatus('to prepare')
+        })
     }
 
-    const getCancelledOrders = () => {
-
-    }
-
-    // This will only run once for the first render.
+    /** 
+     * For Every Refresh,
+     * we need to check if the session
+     * storage is not empty. */
     useEffect(() => {
-        getPendingOrders()
+        
+        let id_number = sessionStorage.getItem('idNumber')
+
+        if (id_number) {
+            setUserIDNumber(id_number);
+            setUser(true)
+        }
     }, [])
+    
+
+    useEffect(() => {
+
+        document.title = 'PNC Cafe | Your Orders'
+        getOrdersWithStatus('to prepare')
+    }, [isUser])
+
+    // 
     return (
-        <div className="flex flex-col gap-3 items-stretch justify-start sm:max-w-md w-full md:max-w-lg mx-auto p-2 md:py-5">
+        <div className="h-max flex flex-col gap-3 items-stretch justify-start sm:max-w-md w-full md:max-w-2xl mx-auto p-2 py-10 md:py-5">
 
             <h1 className="text-2xl font-bold text-pnc lg:text-4xl">Your Orders</h1>
 
             <div className="tabs tabs-boxed">
 
+                {/* To Prepare */}
                 <a onClick={() => {
                     
-                    setActiveTab({pending: true, cancelled: false, received: false})
-                    getPendingOrders()
+                    setActiveTab({toPrepare: true, preparing: false, toReceived: false, received: false, cancelled: false})
+                    getOrdersWithStatus('to prepare')
                 }} 
-                className={`tab ${activeTab.pending ? "tab-style" : ""}`}>Pending</a> 
+                className={`tab ${activeTab.toPrepare ? "tab-style" : ""}`}>To Prepare</a> 
+                
+                {/* Preparing */}
+                <a onClick={() => {
+                    
+                    setActiveTab({toPrepare: false, preparing: true, toReceived: false, received: false, cancelled: false})
+                    getOrdersWithStatus('preparing')
+                }} 
+                 className={`tab ${activeTab.preparing ? "tab-style" : ""}`}>Preparing</a> 
+                
+                {/* To Received */}
+                <a onClick={() => {
+                    
+                    setActiveTab({toPrepare: false, preparing: false, toReceived: true, received: false, cancelled: false})
+                    getOrdersWithStatus('to received')
+                }}  className={`tab ${activeTab.toReceived ? "tab-style" : ""}`}>To Received</a>
 
-                <a onClick={() => setActiveTab({pending: false, cancelled: true, received: false})} 
-                 className={`tab ${activeTab.cancelled ? "tab-style" : ""}`}>Cancelled</a> 
+                {/* Received */}
+                <a onClick={() => {
+                    
+                    setActiveTab({toPrepare: false, preparing: false, toReceived: false, received: true, cancelled: false})
+                    getOrdersWithStatus('received')
+                }}  className={`tab ${activeTab.received ? "tab-style" : ""}`}>Received</a>
 
-                <a onClick={() => setActiveTab({pending: false, cancelled: false, received: true})}  className={`tab ${activeTab.received ? "tab-style" : ""}`}>Received</a>
+                {/* Cancelled */}
+                <a onClick={() => {
+                    
+                    setActiveTab({toPrepare: false, preparing: false, toReceived: false, received: false, cancelled: true})
+                    getOrdersWithStatus('cancelled')
+                }}  className={`tab ${activeTab.cancelled ? "tab-style" : ""}`}>Cancelled</a>
             </div>
+
             {/* Orders Container */}
             <div className="flex flex-col items-stretch gap-3 w-full">
 
                 {
-                    orders.map(order => {
+                    orders.length > 0 ? orders.map(order => {
 
                         return (
-
-                            <div className="flex flex-col gap-4 justify-between sm:flex-row sm:items-center w-full border border-gray-400 p-2 ">
+                            
+                            <div key={order.order_id} className="border-pnc border shadow-lg flex flex-col gap-4 justify-between sm:flex-row sm:items-center w-full p-2 ">
                                 <section>
-                                    <h1 className="font-medium">Order ID: {order.order_id}</h1>
-                                    <h3 className="font-medium">Order Date: {order.order_date}</h3>
+                                    <h1 className="font-medium"><span className="text-pnc">Order ID:</span> {order.order_id}</h1>
+                                    <h3 className="font-medium"><span className="text-pnc">Order Date:</span> {order.order_date}</h3>
                                 </section>
 
                                 <section className="flex items-center">
-                                    <Button className="button-sm" text="Receive"/>
-                                    <Button className="button-no-color" text="View"/>
+
+                                   {
+                                        activeTab.toReceived === true && 
+                                        <Button onClick={() => {
+                                            setOrderStatusAs('received', order.order_id)
+                                        }} className="button-sm" text="Received"/>
+                                   }
+                                   { 
+                                        activeTab.toReceived !== true && 
+                                        activeTab.preparing !== true && 
+                                        activeTab.received !== true && 
+                                        activeTab.cancelled !== true && 
+                                        <Button onClick={() => {
+                                            setOrderStatusAs('cancelled', order.order_id)
+                                        }} 
+                                        className="button-sm-red" 
+                                        text="Cancel"/> 
+                                   }
+
+                                    <label onClick={() => {
+                                        
+                                        setOrderDetails(order)               
+                                    }} htmlFor="invoice" className="text-white button-no-color modal-button" > View </label>
                                     
-                                </section>
+                                </section> 
                             </div>
                         )
-                    })
-                    
+                    }) : 
+                    <div className="flex justify-center items-center">
+                        <h1 className="text-pnc font-bold text-2xl">Empty Orders</h1>
+                    </div>
                 }
-                
+                <UserProvider>
+                    <Invoice orderDetails={orderDetails}/>
+                </UserProvider>
             </div>
         </div>
     )
--- src/components/Invoice.js
+++ src/components/Invoice.js
@@ -3,32 +3,53 @@ import CheckedOutItem from '../components/CheckedOutItem'
 
 function Invoice({orderDetails, referenceFor="invoice"}) {
     
+    const [userInfo, setUserInfo] = useState([])
     const [subTotal, setSubTotal] = useState(0);
-    let total = 0;
 
-    useEffect(() => {
-
-        if (orderDetails !== null) {
-            console.log(orderDetails) // for testing
-            JSON.parse(orderDetails.items).forEach(item => {
-
-                total += item.menuPrice * item.quantity;
-            })
-        }
-
-        setSubTotal(total)
-    })
-
+    /** Formats the price into Philippine Peso */
     const formattedPrice = (price) => {
         return new Intl.NumberFormat('en-IN', {style: 'currency', currency: 'PHP'}).format(price)
     }
 
+    /** Gets the user info
+     * based on the specified user ID number. */
+    const getUserInfo = () => {
+
+        fetch('http://localhost:3001/user-id', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({userIDNumber: orderDetails.id_number})
+        })
+        .then(res => res.json())
+        .then(data => setUserInfo(JSON.parse(data)))
+        
+    }
+
+    /** For every render
+     * compute the subtotal */
+    useEffect(() => {
+
+        let total = 0;
+        
+        if (orderDetails !== null) {
+
+            // Loop for each item and get the menuPrice and quantity properties
+            JSON.parse(orderDetails.items).forEach(item => {
+
+                total += item.menuPrice * item.quantity;
+            })
+            getUserInfo(); // we put this here to avoid null pointer exception
+            
+        }
+        setSubTotal(total)
+    }, [orderDetails])
+
     return (
         <>
             <input type="checkbox" className="modal-toggle" id={referenceFor}/>
-            <div className="modal">
+            <div key={orderDetails !== null && orderDetails.order_id} className="modal">
 
-                <div className="modal-box flex flex-col items-stretch gap-3">
+                <div className="modal-box flex flex-col items-stretch gap-3 max-h-96 overflow-auto md:max-h-full">
                     <h1 className="text-pnc text-2xl lg:text-3xl">Invoice</h1>
 
                     {/* Order Details */}
@@ -39,19 +60,39 @@ function Invoice({orderDetails, referenceFor="invoice"}) {
                             <p><span className="text-pnc font-medium">Order ID:</span> {orderDetails !== null && orderDetails.order_id}</p>
                             <p><span className="text-pnc font-medium">Order Date:</span> {orderDetails !== null && orderDetails.order_date}</p>
                         </section>
+
+                        <section>
+                            {/* Payment Method */}
+                            <p><span className="text-pnc font-medium">Payment Method:</span> {orderDetails !== null && JSON.parse(orderDetails.order_details).paymentMethod}</p>
+
+                            {/* Desired Time */}
+                            <p><span className="text-pnc font-medium">Desired Time:</span> {orderDetails !== null && JSON.parse(orderDetails.order_details).desiredTime}</p>
+
+                            {/* Location */}
+                            <p><span className="text-pnc font-medium">Location:</span> {orderDetails !== null && JSON.parse(orderDetails.order_details).location || 'N/A'}</p>
+                        </section>
                     </div>
+
+                    {/* Customer Info */}
+                    { userInfo.length > 0 && <div className="flex flex-col items-stretch gap-3">
+
+                        <section>
+                            <p><span className="text-pnc font-medium">To:</span> {`${userInfo[0].given_name} ${userInfo[0].middle_name} ${userInfo[0].last_name}`} </p>
+                        </section>
+                        
+                    </div> }
 
                     {/* Items */}
                     <div className="flex flex-col items-stretch gap-3">
-                        <h1 className="text-pnc text-2xl lg:text-3xl">Items</h1>
+                        <h1 className="text-pnc font-medium text-xl">Items</h1>
 
                          <div className="flex flex-col items-stretch pb-3 border-pnc border-b border-dashed">
                             {
                                 orderDetails !== null && JSON.parse(orderDetails.items).map(item => {
-                                    console.log(item)
-                                    
+    
                                     return (
-                                        <CheckedOutItem key={item.orderID} menu={item.menu} quantity={item.quantity} price={item.menuPrice}/>
+                                        // The key is the menuID
+                                        <CheckedOutItem key={item.menuID} menu={item.menu} quantity={item.quantity} price={item.menuPrice}/>
                                     )
                                     
                                 })
