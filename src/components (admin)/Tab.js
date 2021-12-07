ed29bcb1dd403e5bea73d21b137d021f60d9f283
Adrian Nads <marcelonads@gmail.com>
2021-12-07 11:20:07 +0800

    Added a TabItem component at Tab.js to organized the code

--- src/components (admin)/Tab.js
+++ src/components (admin)/Tab.js
@@ -3,31 +3,39 @@ import {Link} from 'react-router-dom'
 
 function Tab() {
 
+    /** A state which determines
+     * which tab is currently active/selected. */
     const [activeTab, setActiveTab] = useState({
 
         user: true,
         menu: false,
-        transaction: false
+        transaction: false,
+        orders: false
     })
 
     return (
-        <div className="my-2 tabs row-start-2 row-end-3 col-start-1 col-end-5 w-screen">
-            <Link onClick={() => {
+        <div className="my-2 tabs tabs-boxed row-start-1 row-end-2 col-start-1 col-end-5 w-screen">
 
-                setActiveTab({user: true, menu: false, transaction: false})
-            }} to="/admin/users" className={`text-pnc font-bold tab tab-lifted tab-bordered tab-border-2 ${activeTab.user ? "tab-active" : ""}`}>Users</Link> 
-
-            <Link onClick={() => {
-
-                setActiveTab({user: false, menu: true, transaction: false})
-            }} to="/admin/menu" className={`text-pnc font-bold tab tab-lifted tab-bordered tab-border-2 ${activeTab.menu ? "tab-active" : ""}`}>Menu</Link> 
-
-            <Link onClick={() => {
-
-                setActiveTab({user: false, menu: false, transaction: true})
-            }} to="/admin/menu" className={`text-pnc font-bold tab tab-lifted tab-bordered tab-border-2 ${activeTab.transaction ? "tab-active" : ""}`}>Transaction</Link>
+            <TabItem activeTab={activeTab} setActiveTab={setActiveTab} linkRef={"/admin/users"} 
+                tabItemText={"Users"} newActiveTab={{user: true, menu: false, transaction: false, orders: false}} tabName={"user"}/>
+            <TabItem activeTab={activeTab} setActiveTab={setActiveTab} linkRef={"/admin/menu"} 
+                tabItemText={"Menu"} newActiveTab={{user: false, menu: true, transaction: false, orders: false}} tabName={"menu"}/>
+            <TabItem activeTab={activeTab} setActiveTab={setActiveTab} linkRef={"/admin/menu"} 
+                tabItemText={"Transactions"} newActiveTab={{user: false, menu: false, transaction: true, orders: false}} tabName={"transaction"}/>
+            <TabItem activeTab={activeTab} setActiveTab={setActiveTab} linkRef={"/admin/orders"} 
+                tabItemText={"Orders"} newActiveTab={{user: false, menu: false, transaction: false, orders: true}} tabName={"orders"}/>
         </div>
     )
 }
 
+function TabItem ({activeTab, setActiveTab, linkRef, tabItemText, newActiveTab, tabName}) {
+
+    return (
+    
+        <Link onClick={() => {
+
+            setActiveTab(newActiveTab)
+        }} to={linkRef} className={`text-pnc font-bold tab ${activeTab[tabName] ? "tab-style" : ""}`}>{tabItemText}</Link> 
+    )
+}
 export default Tab
