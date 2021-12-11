3f53a616c17be69fccf835e0920d6cd68066ad8e
Adrian Nads <marcelonads@gmail.com>
2021-12-11 15:46:34 +0800

    Add an authentication at AdminLoginPage

--- src/Pages/AdminLogin.js
+++ src/Pages/AdminLogin.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import Textfield from '../components/Textfield'
 import Label from '../components/Label'
 import Button from '../components/Button'
+import ErrorAlert from '../components/ErrorAlert'
 
 function AdminLogin({setAdmin}) {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [isError, setError] = useState(false)
+
+    useEffect(() => {
+
+        document.title = 'PNC Cafe | Admin'
+    }, [])
+
+    // A function that verifies the admin.
+    const verifyAdmin = () => {
+
+        fetch('http://localhost:3001/verify-admin', { 
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({username, password})
+        })
+        .then(response => response.json())
+        .then(data => {
+
+            console.log(JSON.parse(data))
+            if (JSON.parse(data).length > 0) {
+                setAdmin(true);
+                window.sessionStorage.setItem('isAdmin', 'true');
+                window.location.replace('http://localhost:3000/admin/users')
+            } else {
+                setError(true)
+            }
+        })
+    }
+
     return (
         <div className="hero w-screen">
         
@@ -15,29 +48,40 @@ function AdminLogin({setAdmin}) {
                     <div className="p-0 md:p-8">
                         
                         <h1 className="card-title">Hello, Admin</h1>
-                        <form action="">
+                        <form action="" onSubmit={(e) => {
+
+                            e.preventDefault()
+
+                            verifyAdmin()
+                        }}>
 
                             <section className="input-container flex flex-col relative">
-                                <Textfield props={{type: "text", name: "username"}}/>
+                                <Textfield value={username} onChange={(e) => {
+
+                                    setError(false)
+                                    setUsername(e.target.value);
+                                }} props={{type: "text", name: "username"}}/>
                                 <Label props={{name: "username", labelContent: "Username"}}/>
                             </section>
 
                             <section className="input-container flex flex-col relative">
-                                <Textfield props={{type: "password", name: "password"}}/>
+                                <Textfield value={password} onChange={(e) => {
+
+                                    setPassword(e.target.value)
+                                    setError(false)
+                                }} props={{type: "password", name: "password"}}/>
                                 <Label props={{name: "password", labelContent: "Password"}}/>
                             </section>
 
                             <section className="input-container flex flex-col relative">
-                                <Button onClick={() => {
-                                        setAdmin(true);
-                                        window.sessionStorage.setItem('isAdmin', 'true');
-                                        window.location.assign('http://localhost:3000/admin/users')
-                                    }} className="globalButtons w-full" text="Login"/>
+                                <Button className="globalButtons w-full" text="Login"/>
                             </section>
 
                             <section class="flex flex-col">
                                 <Link className="text-pnc font-medium text-center" to="">Forgot password?</Link>
                             </section>
+
+                            {isError && <ErrorAlert errorMessage={"Incorrect Password"}/>}
                         </form>
                         
                     </div>
--- src/Pages/AdminPage.js
+++ src/Pages/AdminPage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
 import Tab from '../components (admin)/Tab'
-import Sidebar from '../components (admin)/Sidebar'
 
 function AdminPage() {
 
@@ -9,8 +8,6 @@ function AdminPage() {
         <div className="main-content-admin lg:h-full lg:sticky">
             
             <Tab />
-            {/* Sidebar that contains the USER, TRANSACTION, AND MENU BUTTON */}
-            {/* <Sidebar />  */}
 
             {/* This will render the page for user, transaction, and menu table */}
             <Outlet />
